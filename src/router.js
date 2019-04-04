44b94becc75824844d1f9a3fdd57b0b93c5960a6
XiangYu_Hu <luckyhu1988@163.com>
2019-04-04 16:15:03 +0800

    路由配置
    新增404页面

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+import page404 from './views/404.vue';
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,10 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },{
+      path:'*',
+      name: '404',
+      component:page404
     }
   ]
 })
