5737f1da25ec11f6ef7e3a72715a8da3a18b07fe
XiangYu_Hu <luckyhu1988@163.com>
2019-04-04 16:14:40 +0800

    404页面

---
+++ src/views/404.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>404 您访问的页面没有找到</h1>
+</template>
+<script>
+
+  export default {
+      name: 'home',
+      data:function(){
+          return {
+          }
+      },
+      methods:{
+          
+          
+      },
+      mounted () {
+      
+      },
+      created:function(){
+          
+      }
+  }
+</script>
